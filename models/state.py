ecf3d52c54155703bd4805a97b2b135bb8a8c48f
EstebanArchbold <est.archbold.edas@gmail.com>
2021-02-17 22:24:07 -0500

    adding state.py

---
+++ models/state.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+State Public class
+"""
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+    '''
+    State class that inherits from BaseModel class
+    '''
+    name = ""
